af57b1b30577d03ffe93aa9a7b00aa66daf7a683
Man Kit Tse <michael@Mans-MacBook-Air.local>
2020-05-26 00:22:04 +0800

    initial

---
+++ ESP-DHT11_telegram/ESP-DHT11_telegram.ino
@@ -0,0 +1,108 @@
+
+// Import required libraries
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <Hash.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecureBearSSL.h>
+
+
+// Replace with your network credentials
+const char* ssid = "XXXXXXXXXXX";
+const char* password = "XXXXXXXXXXX";
+// Initialize Telegram BOT
+const char* BotKey = "XXXXXXXXXXX";
+const char* chat_id = "XXXXXXXXXXX";
+String text;
+
+// Fingerprint for URL, expiration date = 2022-05-24 00:17:38
+const uint8_t fingerprint[20] = {0xf2, 0xad, 0x29, 0x9c, 0x34, 0x48, 0xdd, 0x8d, 0xf4, 0xcf, 0x52, 0x32, 0xf6, 0x57, 0x33, 0x68, 0x2e, 0x81, 0xc1, 0x90};
+// thresholds
+const float t_threshold = 30.0;
+const float h_threshold = 40.0;
+
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  Serial.setTimeout(2000);
+  dht.begin();
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println(".");
+  }
+  // Print ESP8266 Local IP Address
+  Serial.println(WiFi.localIP());
+
+
+}
+
+void loop() {
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  while (isnan(t) or isnan(h)){
+    Serial.println("Failed to read from DHT sensor!");
+    delay(1000);
+    t = dht.readTemperature();
+    h = dht.readHumidity();
+  }
+  Serial.println(t);
+  Serial.println(h);
+  
+  //alarm function & send to telegram
+  if (t > t_threshold or h > h_threshold) {
+    char t_str[8];
+    char h_str[8];
+    sprintf(t_str, "%2.2f", t);
+    sprintf(h_str, "%2.2f", h);
+    text = String("SomeThing Wrong! Temp: ") + t_str + "; RH: " + h_str;
+    if (WiFi.status() == WL_CONNECTED) {
+      std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+  
+      client->setFingerprint(fingerprint);
+  
+      HTTPClient https;
+      String serverPath = String("https://api.telegram.org/bot") + BotKey + "/sendMessage?chat_id=" + chat_id + "&text=" + text;
+      // Your Domain name with URL path or IP address with path
+      https.begin(*client, serverPath);
+  
+      // Send HTTP GET request
+      int httpResponseCode = https.GET();
+      String payload = https.getString();
+  
+      if (httpResponseCode > 0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+        Serial.println(payload);
+      }
+      https.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+  }
+  
+  //ESP.deepSleep(30e6);         //30s
+  ESP.deepSleep(3600e6);   //1 hr (max71mins)
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # StandAlone_T-H_sensor
-Monitor the temperature and relative humidity condition in the Dry Box
+Monitor the temperature and relative humidity condition in a enclosed area, such as a dry box, with a battery.
+Once the reading is over the limit, a message will be sent through a telegram bot.
+This version is using a ESP8266 NodeMCU and ESP01 with DHT11 digital humidity temperature sensor, but it can be burn into any ESP8266 board with DHT22/DHT21 sensor.
+
+## Configuration
+
+There are a few parameters that need to be changed:
+ssid : SSID of your WIFI connection
+password : Password of your WIFI connection
+BotKey : Telegram Bot Token
+chat_id : chat ID to indentify the chatting client (you)
+fingerprint : fingerprint of https://api.telegram.org/, need to update time to time
+t_threshold : temperature threshold, will send out a message if over this limit
+h_threshold : humidity threshold, will send out a message if over this limit
+
+### Prerequisites
+
+Software: Arduino IDE, CH34x driver (optional)
+Hardware: ESP8266 NodeMCU, DHT11, Power Bank with 5V USB output
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
