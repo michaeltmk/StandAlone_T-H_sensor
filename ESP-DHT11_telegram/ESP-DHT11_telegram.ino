fee2698b54280144b8f26f23df0474163dc64753
Man Kit Tse <michael@Mans-MacBook-Air.local>
2020-08-01 21:56:42 +0800

    disable wifi when not use

--- ESP-DHT11_telegram/ESP-DHT11_telegram.ino
+++ ESP-DHT11_telegram/ESP-DHT11_telegram.ino
@@ -11,20 +11,22 @@
 
 
 // Replace with your network credentials
-const char* ssid = "XXXXXXXXXXX";
-const char* password = "XXXXXXXXXXX";
+const char* ssid = "XXX";
+const char* password = "XXX";
+
 // Initialize Telegram BOT
-const char* BotKey = "XXXXXXXXXXX";
-const char* chat_id = "XXXXXXXXXXX";
+const char* BotKey = "XXX";
+const char* chat_id = "XXX";
 String text;
 
 // Fingerprint for URL, expiration date = 2022-05-24 00:17:38
 const uint8_t fingerprint[20] = {0xf2, 0xad, 0x29, 0x9c, 0x34, 0x48, 0xdd, 0x8d, 0xf4, 0xcf, 0x52, 0x32, 0xf6, 0x57, 0x33, 0x68, 0x2e, 0x81, 0xc1, 0x90};
 // thresholds
-const float t_threshold = 30.0;
-const float h_threshold = 40.0;
+const float t_threshold = 35.0;
+const float h_threshold = 35.0;
 
 #define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DHTPOWPIN 5   // power to DHT sensor
 
 // Uncomment the type of sensor in use:
 #define DHTTYPE    DHT11     // DHT 11
@@ -37,18 +39,16 @@ void setup() {
   // Serial port for debugging purposes
   Serial.begin(115200);
   Serial.setTimeout(2000);
+  
+  // disable WiFi, coming from DeepSleep, as we do not need it right away
+  WiFi.mode( WIFI_OFF );
+  WiFi.forceSleepBegin();
+  delay( 1 );
+
+  pinMode (DHTPOWPIN, OUTPUT);   //output mode
+  digitalWrite(DHTPOWPIN, HIGH);   //turn on DHT sensor
+  delay( 100 );
   dht.begin();
-
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println(".");
-  }
-  // Print ESP8266 Local IP Address
-  Serial.println(WiFi.localIP());
-
 
 }
 
@@ -72,6 +72,17 @@ void loop() {
     sprintf(t_str, "%2.2f", t);
     sprintf(h_str, "%2.2f", h);
     text = String("SomeThing Wrong! Temp: ") + t_str + "; RH: " + h_str;
+
+    // Connect to Wi-Fi
+    WiFi.begin(ssid, password);
+    Serial.println("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.println(".");
+    }
+    // Print ESP8266 Local IP Address
+    Serial.println(WiFi.localIP());
+
     if (WiFi.status() == WL_CONNECTED) {
       std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
   
@@ -102,7 +113,7 @@ void loop() {
       Serial.println("WiFi Disconnected");
     }
   }
-  
+
   //ESP.deepSleep(30e6);         //30s
   ESP.deepSleep(3600e6);   //1 hr (max71mins)
 }
